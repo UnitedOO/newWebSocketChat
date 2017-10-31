9b0eabe928ca5058453760fddb203edc9bcd9e1f
OlgaKostiuk <olga.d.kostiuk@gmail.com>
2017-10-31 21:12:45 +0200

    Add project files.

---
+++ WebSocketClient/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WebSocketClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chatBox
+            // 
+            this.chatBox.FormattingEnabled = true;
+            this.chatBox.Location = new System.Drawing.Point(13, 13);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(259, 121);
+            this.chatBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your message:";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(16, 178);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(256, 20);
+            this.txtSend.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(150, 204);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(33, 203);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chatBox);
+            this.Name = "Form1";
+            this.Text = "Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox chatBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ WebSocketClient/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace WebSocketClient
+{
+    public partial class Form1 : Form
+    {
+        private WebSocketSharp.WebSocket client;
+        const string host = "ws://localhost:8080/Server";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new WebSocketSharp.WebSocket(host);
+
+            client.OnOpen += (s, e) => chatBox.Items.Add("Connect is open.");
+            client.OnError += (s, e) => chatBox.Items.Add("Error: " + e.Message);
+            client.OnMessage += (s, e) => chatBox.Items.Add("Server: " + e.Data);
+            client.OnClose += (s, e) => chatBox.Items.Add("Connect is close.");
+
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Connect();
+            }
+            catch
+            {
+                chatBox.Items.Add("Cannot connect to server!");
+            }
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var content = txtSend.Text;
+                if (!string.IsNullOrEmpty(content))
+                    client.Send(content);
+            }
+            catch
+            {
+                chatBox.Items.Add("Cannot connect to server!");
+            }
+        }
+       
+    }
+}
